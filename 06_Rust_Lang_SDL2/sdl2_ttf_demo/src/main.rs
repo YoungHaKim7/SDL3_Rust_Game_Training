220df1fcbca56acf801a618b3873e335a6991f23
GYoung <ytok1108@aol.com>
2024-12-15 17:00:20 +0900

    ttf 글씨 +  Macro공부가 굿

--- 06_Rust_Lang_SDL2/sdl2_ttf_demo/src/main.rs
+++ 06_Rust_Lang_SDL2/sdl2_ttf_demo/src/main.rs
@@ -1,13 +1,8 @@
 extern crate sdl2;
 
-use std::env;
-use std::path::Path;
+use std::{env, path::Path};
 
-use sdl2::event::Event;
-use sdl2::keyboard::Keycode;
-use sdl2::pixels::Color;
-use sdl2::rect::Rect;
-use sdl2::render::TextureQuery;
+use sdl2::{event::Event, keyboard::Keycode, pixels::Color, rect::Rect, render::TextureQuery};
 
 static SCREEN_WIDTH: u32 = 800;
 static SCREEN_HEIGHT: u32 = 600;
