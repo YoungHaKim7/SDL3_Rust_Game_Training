fe68cf3cab53fe1c670821a3f418df0c7528c6cf
GYoung <ytok1108@aol.com>
2025-01-31 23:38:05 +0900

    SDL2(Rust) tetris 만들기 Let's go 01

---
+++ 06_Rust_Lang_SDL2/02_sdl2_rust_tetris/01_tetris/a01_tetris/src/interface/mod.rs
@@ -0,0 +1,12 @@
+use crate::engine::Engine;
+
+pub struct Interface {
+    engine: Engine,
+}
+
+impl Interface {
+    pub fn run(engine: Engine) {
+        let interface = Self { engine };
+        todo!("Run the game")
+    }
+}
